76269502f4995da8eb7337777f2d20b046ee4125
Christian Klein C. Ramos <ckcr20@gmail.com>
2024-12-29 09:39:06 +0800

    init

---
+++ EEE158_MP2/build/default/production/platform/gpio.o.d
@@ -0,0 +1,6 @@
+build/default/production/platform/gpio.o: platform/gpio.c \
+ platform/blink_settings.h platform/../platform.h
+
+platform/blink_settings.h:
+
+platform/../platform.h:
---
+++ EEE158_MP2/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=EEE158_MP2.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/EEE158_MP2.production.hex
